15de4903bc0734a7dba75b594aa7b98a45954ad7
naiyoma <lankas.aurelia@gmail.com>
2018-09-03 01:36:28 +0300

    renaming the static folder

--- app/models.py
+++ app/models.py
@@ -19,3 +19,6 @@ class Articles:
         self.url = url 
         self.urlToImage = urlToImage
         self.publishedAt = publishedAt
+
+# 1.i have created a news class that will hold the responses from the the api call
+# 2.The def__init__method means that this clases can be imported anywhere
